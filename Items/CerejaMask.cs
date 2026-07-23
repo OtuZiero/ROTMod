6e52ecc796dc9e5939ec69c36f452203a315a74c
Cejera <132576561+cejera@users.noreply.github.com>
2026-07-23 18:37:59 -0300

    Fix: Remove AutoloadEquip from CerejaMask - it's a consumable item, not an equippable

--- Items/CerejaMask.cs
+++ Items/CerejaMask.cs
@@ -4,16 +4,14 @@ using Terraria.ModLoader;
 
 namespace ROTMod.Items
 {
-    [AutoloadEquip(EquipType.Head)]
     public class CerejaMask : ModItem
     {
         public override void SetDefaults()
         {
-            Item.width = 28;
-            Item.height = 28;
-            Item.defense = 32;
-            Item.rare = ItemRarityID.Red;
-            Item.value = Item.sellPrice(gold: 10);
+            Item.width = 20;
+            Item.height = 20;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = Item.sellPrice(silver: 50);
         }
     }
 }
